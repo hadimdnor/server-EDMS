b6776c473133ab8b0fce8d4cfdbae2a7e016b462
hadimdnor <hadimdnor21@gmail.com>
2021-01-28 13:31:32 +0800

    updated server file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pg = require('pg');
 const port = process.env.PORT || 3001;
 
 
-const origin = process.env.PRODUCTION ? "http://edms.surge.sh/" : "http://localhost:3000"
+const origin = process.env.PRODUCTION ? "http://edms.surge.sh" : "http://localhost:3000"
 
 app.use(cors({ origin }));
 app.use(express.json());
